23893d954fbe62d4681c8246ba7e4cc10f32e5fe
Len Smith <len@barrison.com>
2016-01-18 16:33:05 -0800

    Episode 1

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 export default class App extends Component {
+  static propTypes = {
+    children: React.PropTypes.object
+  };
+
   render() {
-    return (<div>Hello Universal Redux!</div>);
+    return (
+      <div>
+        <div id="nav">
+          <Link to="/">Home</Link>&nbsp;
+          <Link to="/sessions">Sessions</Link>
+        </div>
+
+        { this.props.children }
+      </div>
+    );
   }
 }
