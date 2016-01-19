c72dbecf096b04d74fff0cbf01022bc64a6d0376
Len Smith <len@barrison.com>
2016-01-18 16:33:05 -0800

    Episode 1

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Route} from 'react-router';
 import App from './containers/App/App';
+import { Sessions } from './components';
 
 export default () => {
   return (
-    <Route path="/" component={App} />
+    <Route path="/" component={App} >
+      <Route path="/sessions" component={Sessions}/>
+    </Route>
   );
 };
